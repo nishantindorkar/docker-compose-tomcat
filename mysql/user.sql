a498947691510478c5591868718b14253537d6d9
nishant <nyindorkar@gmail.com>
2023-02-27 17:41:25 +0530

    two

--- mysql/user.sql
+++ mysql/user.sql
@@ -1,13 +1,17 @@
-CREATE DATABASE studentapp;
-    use studentapp;
-    CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
-        student_name VARCHAR(100) NOT NULL,
-        student_addr VARCHAR(100) NOT NULL,
-        student_age VARCHAR(3) NOT NULL,
-        student_qual VARCHAR(20) NOT NULL,
-        student_percent VARCHAR(10) NOT NULL,
-        student_year_passed VARCHAR(10) NOT NULL,
-        PRIMARY KEY (student_id)
-    );
-    ALTER USER 'admin' IDENTIFIED WITH mysql_native_password BY 'Admin123'; 
-    flush privileges;
+CREATE DATABASE IF NOT EXISTS studentapp;
+USE studentapp;
+
+CREATE TABLE IF NOT EXISTS students (
+    student_id INT NOT NULL AUTO_INCREMENT,
+    student_name VARCHAR(100) NOT NULL,
+    student_addr VARCHAR(100) NOT NULL,
+    student_age VARCHAR(3) NOT NULL,
+    student_qual VARCHAR(20) NOT NULL,
+    student_percent VARCHAR(10) NOT NULL,
+    student_year_passed VARCHAR(10) NOT NULL,
+    PRIMARY KEY (student_id)
+);
+
+ALTER USER 'admin' IDENTIFIED WITH mysql_native_password BY 'Admin123'; 
+
+FLUSH PRIVILEGES;
